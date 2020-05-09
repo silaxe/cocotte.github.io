1cf05d3e476524215c35dbf4f19cbadd10bbdd7e
snipat <saint-patrick@hotmail.fr>
2020-05-09 16:47:20 +0200

    orientaiton

--- script_alone.js
+++ script_alone.js
@@ -1,23 +1,23 @@
 window.onload = function () {
   // Check if is IOS 13 when page loads. Solution Aframe
-  if ( window.DeviceMotionEvent && typeof window.DeviceMotionEvent.requestPermission === 'function' ){
+  if ( window.DeviceOrientationEvent && typeof window.DeviceOrientationEvent.requestPermission === 'function' ){
       // Everything here is just a lazy banner. You can do the banner your way.
       const banner = document.createElement('div')
-      banner.innerHTML = `<div style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff"><p style="padding: 10px">Click here to enable DeviceMotion</p></div>`
-      banner.onclick = ClickRequestDeviceMotionEvent // You NEED to bind the function into a onClick event. An artificial 'onClick' will NOT work.
+      banner.innerHTML = `<div style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff"><p style="padding: 10px">Click here to enable DeviceOrientation</p></div>`
+      banner.onclick = ClickRequestDeviceOrientationEvent // You NEED to bind the function into a onClick event. An artificial 'onClick' will NOT work.
       document.querySelector('body').appendChild(banner)
   }
 
-  function ClickRequestDeviceMotionEvent () {
-    window.DeviceMotionEvent.requestPermission()
+  function ClickRequestDeviceOrientationEvent () {
+    window.DeviceOrientationEvent.requestPermission()
       .then(response => {
         if (response === 'granted') {
           alert("oui");
-          window.addEventListener('devicemotion',
-            () => { console.log('DeviceMotion permissions is granted.') },
+          window.addEventListener('deviceorientation',
+            () => { console.log('DeviceOrientation permissions is granted.') },
             (e) => { throw e }
         )} else {
-          console.log('DeviceMotion permissions not granted.')
+          console.log('DeviceOrientation permissions not granted.')
           alert("Non")
         }
       })
