3cb63e4e6fe5152d5cd550dfc9e407f62841ad05
snipat <saint-patrick@hotmail.fr>
2020-05-15 09:51:40 +0200

    Push du script fonctionnel

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function () {
   if ( window.DeviceOrientationEvent && typeof window.DeviceOrientationEvent.requestPermission === 'function' ){
       // Everything here is just a lazy banner. You can do the banner your way.
       const banner = document.createElement('div')
-      banner.innerHTML = `<div style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff"><p style="padding: 10px">Click here to enable DeviceOrientation</p></div>`
+      banner.innerHTML = `<div id="autorisation" style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff"><p style="padding: 10px">Cliquez ici pour autoriser l'accès à votre capteur de mouvements.</p></div>`
       banner.onclick = ClickRequestDeviceOrientationEvent // You NEED to bind the function into a onClick event. An artificial 'onClick' will NOT work.
       document.querySelector('body').appendChild(banner)
   }
@@ -12,16 +12,15 @@ window.onload = function () {
     window.DeviceOrientationEvent.requestPermission()
       .then(response => {
         if (response === 'granted') {
-          alert("oui");
           window.addEventListener('deviceorientation',function(e) {
+    document.getElementById('autorisation').style.display = 'none';
     document.getElementById('alpha').innerHTML = Math.round(e.alpha);
     document.getElementById('beta').innerHTML = Math.round(e.beta) ;
     document.getElementById('gamma').innerHTML = Math.round(e.gamma) ;
   }
 //document.getElementById('acceleration-including-gravity-x').innerHTML = Math.round(event.accelerationIncludingGravity.x);
           )} else {
-          console.log('DeviceOrientation permissions not granted.')
-          alert("Non")
+          alert("Désolé, vous ne pouvez pas jouer à ce jeu car votre appareil n'a pas de capteur de mouvement.")
         }
       })
       .catch(e => {
