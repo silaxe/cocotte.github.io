f5c572952ee9c0c95f0c71ae6f00c7de8f4e975d
silaxe <athuau@gmail.com>
2020-05-22 12:06:06 +0200

    boost

--- script_alone.js
+++ script_alone.js
@@ -79,11 +79,11 @@ document.getElementById('incTangage').innerHTML = incTangage;
       incTangage = 1;
     }
   }
-//  function incrementValue()
-//  {
-//      var value = parseInt(document.getElementById('boost').value, 10);
-//      value = isNaN(value) ? 0 : value;
-//      value++;
-//      document.getElementById('boost').value = value;
-//  }
+  function incrementValue()
+  {
+      var value = parseInt(document.getElementById('boost').value, 10);
+      value = isNaN(value) ? 0 : value;
+      value++;
+      document.getElementById('boost').value = value;
+  }
 }
