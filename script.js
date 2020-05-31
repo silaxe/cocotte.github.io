3ded130e204216427cdba3f4b3ea11e3a0a5f7ad
silaxe <athuau@gmail.com>
2020-05-31 14:17:55 +0200

    commit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let beta, gamma, pression=0, gameover=false;
 
-// window.onload = function () {
-//   if ( window.DeviceOrientationEvent && typeof window.DeviceOrientationEvent.requestPermission === 'function' ){
-//       const banner = document.createElement('div');
-//      banner.innerHTML = `<div id="autorisation" style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff"><p style="padding: 10px">Cliquez ici pour autoriser l'accès à votre capteur de mouvements.</p></div>`;
-//      banner.onclick = clickRequestDeviceOrientationEvent;
-//      document.querySelector('body').appendChild(banner)
-//  }
-// }
+window.onload = function () {
+   if ( window.DeviceOrientationEvent && typeof window.DeviceOrientationEvent.requestPermission === 'function' ){
+       const banner = document.createElement('div');
+      banner.innerHTML = `<div id="autorisation" style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff"><p style="padding: 10px">Cliquez ici pour autoriser l'accès à votre capteur de mouvements.</p></div>`;
+      banner.onclick = clickRequestDeviceOrientationEvent;
+      document.querySelector('body').appendChild(banner)
+  }
+ }
 
 function clickRequestDeviceOrientationEvent() {
 
@@ -21,7 +21,7 @@ function clickRequestDeviceOrientationEvent() {
             gamma=(Math.round(e.gamma));
             increasePression();
             changeColor();
-            window.location.href=#game;
+            //window.location.href=#game;
                           }
           )} else {
           alert("Désolé, vous ne pouvez pas jouer à ce jeu car votre appareil n'a pas de capteur de mouvement.")
