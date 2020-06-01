8fc9895af3cf9b70f0f0dc0578a0de8b7c0942e0
silaxe <athuau@gmail.com>
2020-06-01 12:52:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let beta, gamma, pression=0, gameover=false, audio_source;
 function bannerAuthorisation() {
    if (window.DeviceOrientationEvent && typeof window.DeviceOrientationEvent.requestPermission === 'function'){
       const banner = document.createElement('div');
-      banner.innerHTML = `<div id="autorisation" style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff" onclick="clickRequestDeviceOrientationEvent(); lvlSound();"><p style="padding: 10px">Cliquez ici pour autoriser l'accès à votre capteur de mouvements.</p></div>`;
+      banner.innerHTML = `<div id="autorisation" style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff" onclick="clickRequestDeviceOrientationEvent()"><p style="padding: 10px">Cliquez ici pour autoriser l'accès à votre capteur de mouvements.</p></div>`;
 //      banner.onclick = clickRequestDeviceOrientationEvent();
       document.querySelector('body').appendChild(banner)
 }
@@ -19,6 +19,14 @@ function clickRequestDeviceOrientationEvent() {
             document.getElementById('autorisation').style.display = 'none';
             beta=(Math.round(e.beta));
             gamma=(Math.round(e.gamma));
+        if (document.getElementById('pression') == 0){
+            let audio = new Audio('assets/son_mini.wav');
+            audio.play();
+          }
+        else if (document.getElementById('pression') == 500) {
+            let audio = new Audio('assets/son_medium.wav');
+            audio.play();
+          }
             increasePression();
             changeColor();
             }
@@ -99,23 +107,6 @@ function changeColor () {
     gameover = true;
     }
   }
-
-function lvlSound() {
-//  if (document.getElementById('pression') == 0){
-//  audio_source = "assets/son_mini.wav";
-//}
-//  else if (document.getElementById('pression') == 500) {
-//  audio_source = "assets/son_medium.wav";
-//}
-//  else {
-//  audio_source = "assets/son_hard.wav";
-//}
-    while(pression < 2000) {
-    document.getElementById('son').innerHTML = ('Son : '+pression);
-    pression++;
-//    let audio = new Audio('assets/son_mini.wav');
-//    audio.play();
-}
 
 /*
 Solution pour récupérer les paramètres de jeu depuis la fenêtre Options
