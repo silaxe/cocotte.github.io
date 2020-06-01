d9ccd32841546aef929ec0fbcc95cf7da3c26149
silaxe <athuau@gmail.com>
2020-06-01 13:18:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ function clickRequestDeviceOrientationEvent() {
             document.getElementById('autorisation').style.display = 'none';
             beta=(Math.round(e.beta));
             gamma=(Math.round(e.gamma));
-            increasePression();
             changeColor();
+            increasePression();
             }
           )} else {
           alert("Désolé, vous ne pouvez pas jouer à ce jeu car votre appareil n'a pas de capteur de mouvement.")
