e7392dbe5b4fd9e115d956a0513587699ae61399
silaxe <athuau@gmail.com>
2020-06-01 11:40:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let beta, gamma, pression=0, gameover=false;
 function bannerAuthorisation() {
    if (window.DeviceOrientationEvent && typeof window.DeviceOrientationEvent.requestPermission === 'function'){
       const banner = document.createElement('div');
-      banner.innerHTML = `<div id="autorisation" style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff" onclick="clickRequestDeviceOrientationEvent();"><p style="padding: 10px">Cliquez ici pour autoriser l'accès à votre capteur de mouvements.</p></div>`;
+      banner.innerHTML = `<div id="autorisation" style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff" onclick="clickRequestDeviceOrientationEvent(); playSound();"><p style="padding: 10px">Cliquez ici pour autoriser l'accès à votre capteur de mouvements.</p></div>`;
 //      banner.onclick = clickRequestDeviceOrientationEvent();
       document.querySelector('body').appendChild(banner)
 }
@@ -22,9 +22,7 @@ function clickRequestDeviceOrientationEvent() {
             gamma=(Math.round(e.gamma));
             increasePression();
             changeColor();
-            //changeSound();
-            //window.location.href=#game;
-                          }
+            }
           )} else {
           alert("Désolé, vous ne pouvez pas jouer à ce jeu car votre appareil n'a pas de capteur de mouvement.")
       }
@@ -42,8 +40,6 @@ function clickRequestDeviceOrientationEvent() {
 // function refreshInfo() {
 //  pression=0;
 //}
-
-function resetPression() {}
 
 function increasePression() {
 
@@ -90,7 +86,6 @@ function increasePression() {
   }
 }
 
-
 function changeColor () {
 
   if (pression >= 0 && pression < 500){
@@ -101,12 +96,12 @@ function changeColor () {
     document.getElementById('pression').style.color = "red";
     }
 
-  else if (pression > 2000) {
+  else {
     gameover = true;
     }
   }
 
-function changeSound() {
+function playSound() {
     var audio = new Audio('assets/son_hard.wav');
     audio.play();
 }
