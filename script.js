621064d7467ff258b6c81b61c8efb8f7d2d8efb8
snipat <saint-patrick@hotmail.fr>
2020-06-06 11:20:34 +0200

    ok test

--- script.js
+++ script.js
@@ -4,27 +4,34 @@ let beta, gamma, pression=0, gameover=false, audio_source;
 function bannerAuthorisation() {
    if (window.DeviceOrientationEvent && typeof window.DeviceOrientationEvent.requestPermission === 'function'){
       const banner = document.createElement('div');
-      banner.innerHTML = `<div id="autorisation" style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff" onclick="clickRequestDeviceOrientationEvent(); playAudio('assets/son_mini.wav');"><p style="padding: 10px">Cliquez ici pour autoriser l'accès à votre capteur de mouvements.</p></div>`;
+      banner.innerHTML = `<div id="autorisation" style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff" onclick="clickRequestDeviceOrientationEvent();"><p style="padding: 10px">Cliquez ici pour autoriser l'accès à votre capteur de mouvements.</p></div>`;
 //      banner.onclick = clickRequestDeviceOrientationEvent();
       document.querySelector('body').appendChild(banner)
 }
+else {
+  alert("paie toi un iphone");
+  alert(typeof window.DeviceOrientationEvent);
+  alert(typeof window.DeviceOrientationEvent.requestPermission);
+  alert(typeof DeviceOrientationEvent.requestPermission);
+
   }
+}
  //}
 
 function clickRequestDeviceOrientationEvent() {
   window.DeviceOrientationEvent.requestPermission()
       .then(response => {
         if (response === 'granted') {
-            window.addEventListener('deviceorientation',function (e) {
+            window.addEventListener('deviceorientation', (e) => {
             document.getElementById('autorisation').style.display = 'none';
             beta=(Math.round(e.beta));
             gamma=(Math.round(e.gamma));
             changeColor();
             if (audio_source == "son_mini") { document.getElementById('son').innerHTML = "Son : mini" }
-            if (audio_source == "son_medium") { document.getElementById('son').innerHTML = "Son : medium"; playAudio('assets/son_medium.wav'); }
+            if (audio_source == "son_medium") { document.getElementById('son').innerHTML = "Son : medium"; }
             increasePression();
-            document.getElementById('beta').innerHTML = ('Roulis : '+beta);
-            document.getElementById('gamma').innerHTML = ('Tangage : '+gamma);
+            document.getElementById('roulis').innerHTML = ('Roulis : '+beta);
+            document.getElementById('tangage').innerHTML = ('Tangage : '+gamma);
             document.getElementById('pression').innerHTML = ('Pression : '+pression);
             }
           )} else {
@@ -95,7 +102,7 @@ function changeColor () {
 
   else if (pression > 0 && pression < 500) {
     document.getElementById('pression').style.color = "brown";
-    audio_source = "undefined";
+    audio_source = "son_mini";
     }
 
   else if (pression == 500) {
@@ -105,7 +112,7 @@ function changeColor () {
 
   else if (pression > 500 && pression <= 2000) {
     document.getElementById('pression').style.color = "red";
-    audio_source = "undefined";
+    audio_source = "son_hard";
     }
 
   else {
@@ -113,11 +120,28 @@ function changeColor () {
     }
   }
 
-function playAudio(source) {
-  var audio = new Audio(source);
-  audio.loop = true;
+const audio = document.getElementById("au");
+let enablesound = document.getElementById("autorisation");
+console.log(audio);
+enablesound.addEventListener("click", ()=>{
+  audio.play();
+});
+
+const startPlaying = ()=>{
+  audio.removeEventListener('playing', startPlaying);
+  audio.src = 'https://freesound.org/data/previews/475/475736_4397472-lq.mp3';
   audio.play();
 }
+
+audio.addEventListener('playing', startPlaying);
+audio.addEventListener('error', ()=>{
+  console.log("error");
+});
+
+function test () {
+  document.getElementById('pression').innerHTML = "pression";
+}
+test();
 
 /*
 Solution pour récupérer les paramètres de jeu depuis la fenêtre Options
