676cf45194e3df5d4021bae08cd1cca58fd84e18
snipat <32958589+snipat@users.noreply.github.com>
2020-08-03 11:16:30 +0200

    Retour à la normale 5 juin

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ let beta, gamma, pression=0, gameover=false, audio_source;
 function bannerAuthorisation() {
    if (window.DeviceOrientationEvent && typeof window.DeviceOrientationEvent.requestPermission === 'function'){
       const banner = document.createElement('div');
-      banner.innerHTML = `<div id="autorisation" style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff" onclick="clickRequestDeviceOrientationEvent(); playAudio();"><p style="padding: 10px">Cliquez ici pour autoriser l'accès à votre capteur de mouvements.</p></div>`;
+      banner.innerHTML = `<div id="autorisation" style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff" onclick="clickRequestDeviceOrientationEvent();"><p style="padding: 10px">Cliquez ici pour autoriser l'accès à votre capteur de mouvements.</p></div>`;
 //      banner.onclick = clickRequestDeviceOrientationEvent();
       document.querySelector('body').appendChild(banner)
 }
 else {
-  alert("Essaye avec un iphone");
+  alert("paie toi un iphone");
   alert(typeof window.DeviceOrientationEvent);
   alert(typeof window.DeviceOrientationEvent.requestPermission);
   alert(typeof DeviceOrientationEvent.requestPermission);
@@ -22,7 +22,7 @@ function clickRequestDeviceOrientationEvent() {
   window.DeviceOrientationEvent.requestPermission()
       .then(response => {
         if (response === 'granted') {
-            window.addEventListener('deviceorientation', (e) => {
+            window.addEventListener('deviceorientation',function (e) {
             document.getElementById('autorisation').style.display = 'none';
             beta=(Math.round(e.beta));
             gamma=(Math.round(e.gamma));
@@ -30,9 +30,9 @@ function clickRequestDeviceOrientationEvent() {
             if (audio_source == "son_mini") { document.getElementById('son').innerHTML = "Son : mini" }
             if (audio_source == "son_medium") { document.getElementById('son').innerHTML = "Son : medium"; }
             increasePression();
-            document.getElementById('roulis').innerHTML = ('Roulis : '+beta);
-            document.getElementById('tangage').innerHTML = ('Tangage : '+gamma);
-            document.getElementById('jauge').innerHTML = ('Pression : '+pression);
+            document.getElementById('beta').innerHTML = ('Roulis : '+beta);
+            document.getElementById('gamma').innerHTML = ('Tangage : '+gamma);
+            document.getElementById('pression').innerHTML = ('Pression : '+pression);
             }
           )} else {
           alert("Désolé, vous ne pouvez pas jouer à ce jeu car votre appareil n'a pas de capteur de mouvement.")
@@ -52,14 +52,10 @@ function clickRequestDeviceOrientationEvent() {
 //  pression=0;
 //}
 
-function test (){
-  document.getElementById("difficulté").innerHTML = pression;
-}
-
 function increasePression() {
 
     if (gameover) {
-      document.getElementById('jauge').style.color = "purple";
+      document.getElementById('pression').style.color = "purple";
       document.getElementById('gameover').style.visibility = "visible";
     } else {
       if((beta >= 5 && beta < 10) || (beta <= -5 && beta > -10))
@@ -100,22 +96,22 @@ function increasePression() {
 function changeColor () {
 
   if (pression == 0) {
-    document.getElementById('jauge').style.color = "purple";
+    document.getElementById('pression').style.color = "brown";
     audio_source = "son_mini";
     }
 
-  else if (pression >= 0 && pression < 500) {
-    document.getElementById('jauge').style.color = "green";
+  else if (pression > 0 && pression < 500) {
+    document.getElementById('pression').style.color = "brown";
     audio_source = "son_mini";
     }
 
-  else if (pression >= 500 && pression < 1000) {
-    document.getElementById('jauge').style.color = "orange";
+  else if (pression == 500) {
+    document.getElementById('pression').style.color = "red";
     audio_source = "son_medium";
     }
 
-  else if (pression >= 1000 && pression <= 1999) {
-    document.getElementById('jauge').style.color = "red";
+  else if (pression > 500 && pression <= 2000) {
+    document.getElementById('pression').style.color = "red";
     audio_source = "son_hard";
     }
 
@@ -124,10 +120,12 @@ function changeColor () {
     }
   }
 
-function playAudio() {
 const audio = document.getElementById("au");
+let enablesound = document.getElementById("autorisation");
+console.log(audio);
+enablesound.addEventListener("click", ()=>{
   audio.play();
-); }
+});
 
 const startPlaying = ()=>{
   audio.removeEventListener('playing', startPlaying);
@@ -139,8 +137,6 @@ audio.addEventListener('playing', startPlaying);
 audio.addEventListener('error', ()=>{
   console.log("error");
 });
-
-
 
 /*
 Solution pour récupérer les paramètres de jeu depuis la fenêtre Options
